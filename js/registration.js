364b6dbf73a5a6ea3747fcc4ad59e847e561aa7c
brian <jingjerma@gmail.com>
2017-07-24 13:33:04 -0700

    whoops

--- js/registration.js
+++ js/registration.js
@@ -1,6 +1,6 @@
 // Confirms form submission
 function submitConfirm() {
-    alert("Your form has been submitted. You will now be redirected to a confirmation page. Hope to see you soon!")
+    alert("Your form has been submitted. You will now be redirected to a confirmation page. Hope to see you soon!");
 }
 
 // Redirects user to submit success page once form returns true;
