6fb80ff4c192b302f87413bb5cb8c1f6d24eda09
brian <jingjerma@gmail.com>
2017-07-25 00:17:26 -0700

    changed density

--- js/particles.js
+++ js/particles.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function () {
   particleground(document.getElementById('particles'), {
     dotColor: '#fff',
     lineColor: '#fff',
-    density: 9000   
+    density: 10000   
   });
   var intro = document.getElementById('intro');
   intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
