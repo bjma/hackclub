bc75d7d4d0ba0fe7d9e7b2acc2f400274c121372
brian <jingjerma@gmail.com>
2017-07-28 11:16:44 -0700

    added form validation

--- js/registration.js
+++ js/registration.js
@@ -1,7 +1,16 @@
-// Confirms form submission
-function submitConfirm() {
-    alert("Your form has been submitted. You will now be redirected to a confirmation page. Hope to see you soon!");
-}
+// Checks for valid ID number by grabbing first two indexes of an inputted ID
+function formValidation() {
+    var idInput = document.getElementsByName('idNumber');
+    var idString = idInput.toString();
+    var idNumber = idString.substring(0, 2);
 
-// Redirects user to submit success page once form returns true;
-// INPROGRESS
+    if(idNumber == "71" || idNumber == "81" || idNumber == "53") {
+        alert("Your form has been submitted. You will now be redirected to a confirmation page.");
+
+        return true;
+    } else {
+        alert("Please input a valid ID number. If the first two integers of your ID do not match the school district standard, contact a club leader.");
+
+        return false;
+    }
+}
